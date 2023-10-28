299626d6c99c426daa091fa46d086ed367418250
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2023-10-28 18:21:24 +0300

    add2

---
+++ 3Project3/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace _3Project3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Если первое";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(42, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "января",
+            "февраля",
+            "марта",
+            "апреля",
+            "мая",
+            "июня",
+            "июля",
+            "августа",
+            "сентября",
+            "октября",
+            "ноября",
+            "декабря"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "января";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "это";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "понедельник",
+            "втоорник",
+            "среда",
+            "четверг",
+            "пятница",
+            "суббота",
+            "воскресенье"});
+            this.comboBox2.Location = new System.Drawing.Point(273, 10);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "понедельник";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "то";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "января это - понедельник";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 3Project3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3Project3
+{
+    public partial class Form1 : Form
+    {
+        string month = "января";
+        string day = "понедельник";
+        string[] days = { "воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота" };
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = month + "  это - " + day;
+        }
+
+        private string select_day(int firstDay, int Today)
+        {
+            int num_day = firstDay + Convert.ToInt32(Today % 7);
+            if (num_day >= 7)
+            {
+                return days[num_day - 7];
+            }
+            else
+            {
+                return days[num_day];
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*if (comboBox1.SelectedItem.Equals("апреля") || comboBox1.SelectedItem.Equals("июня") 
+                || comboBox1.SelectedItem.Equals("сентября") || comboBox1.SelectedItem.Equals("ноября"))
+            {
+                numericUpDown1.Maximum = 30;
+            }
+            else if (comboBox1.SelectedItem.Equals("января") || comboBox1.SelectedItem.Equals("марта") 
+                || comboBox1.SelectedItem.Equals("мая") || comboBox1.SelectedItem.Equals("июля") 
+                || comboBox1.SelectedItem.Equals("августа") || comboBox1.SelectedItem.Equals("октября") 
+                || comboBox1.SelectedItem.Equals("декабря"))
+            {
+                numericUpDown1.Maximum = 31;
+            }
+            else if (comboBox1.SelectedItem.Equals("февраля"))
+            {
+                numericUpDown1.Maximum = 28;
+            }
+            else
+            {
+                numericUpDown1.Maximum = 1;
+            }*/
+            month = (string)comboBox1.SelectedItem;
+            label4.Text = month + "  это - " + day;
+            switch (comboBox1.SelectedItem)
+            {
+                case "января":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "февраля":
+                    numericUpDown1.Maximum = 28;
+                    break;
+                case "марта":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "апреля":
+                    numericUpDown1.Maximum = 30;
+                    break;
+                case "мая":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "июня":
+                    numericUpDown1.Maximum = 30;
+                    break;
+                case "июля":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "августа":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "сентября":
+                    numericUpDown1.Maximum = 30;
+                    break;
+                case "октября":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                case "ноября":
+                    numericUpDown1.Maximum = 30;
+                    break;
+                case "декабря":
+                    numericUpDown1.Maximum = 31;
+                    break;
+                default:
+                    numericUpDown1.Maximum = 1;
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            day = select_day(comboBox2.SelectedIndex, Convert.ToInt32(numericUpDown1.Value % 7));
+            label4.Text = month + "  это - " + day;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            day = select_day(comboBox2.SelectedIndex, Convert.ToInt32(numericUpDown1.Value % 7));
+            label4.Text = month + "  это - " + day;
+        }
+    }
+}
