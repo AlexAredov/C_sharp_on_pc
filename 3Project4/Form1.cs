605b45c0c42d8080154b69566c63dc142477b90e
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2023-10-28 19:03:49 +0300

    sfd

--- 3Project4/Form1.cs
+++ 3Project4/Form1.cs
@@ -22,46 +22,37 @@ namespace _3Project4
             return (hb / ht) * 100;
         }
 
-        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        private void render(Label label, double res)
         {
-            double res = HbHt((double)numericUpDown2.Value, (double)numericUpDown3.Value);
-            textBox1.Text = Convert.ToString(res);
             if (res >= 31 && res <= 38)
             {
-                label8.Text = "Результат является нормой";
+                label.Text = "Результат является нормой";
             }
             else
             {
                 if (res < 31)
                 {
-                    label8.Text = "Результат является абсолютной гиперхромией";
+                    label.Text = "Результат является абсолютной гиперхромией";
                 }
                 else
                 {
-                    label8.Text = "Результат является абсолютной гипохромией";
+                    label.Text = "Результат является абсолютной гипохромией";
                 }
             }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            double res = HbHt((double)numericUpDown2.Value, (double)numericUpDown3.Value);
+            textBox1.Text = Convert.ToString(res);
+            render(label8, res);
         }
 
         private void numericUpDown3_ValueChanged(object sender, EventArgs e)
         {
             double res = HbHt((double)numericUpDown2.Value, (double)numericUpDown3.Value);
             textBox1.Text = Convert.ToString(res);
-            if (res >= 31 && res <= 38)
-            {
-                label8.Text = "Результат является нормой";
-            }
-            else
-            {
-                if (res < 31)
-                {
-                    label8.Text = "Результат является абсолютной гиперхромией";
-                }
-                else
-                {
-                    label8.Text = "Результат является абсолютной гипохромией";
-                }
-            }
+            render(label8, res);
         }
     }
 }
