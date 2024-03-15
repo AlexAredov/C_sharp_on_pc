c9999b799e67029b322634c1592cfed5012dd2cf
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2024-03-15 16:25:52 +0300

    asad

---
+++ Project2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        /*[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project2/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Student
+    {
+        private string name;
+        private string surname1;
+        private string surname2;
+        private string group;
+        private int[] academicPerformance;
+        private Random rand = new Random(0);
+
+        public Student(string name = "Ivan", string surname1 = "Ivanov", string surname2, string group)
+        {
+            this.Name = name;
+            this.Surname1 = surname1;
+            this.Surname2 = surname2;
+            this.Group = group;
+            academicPerformance = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                AcademicPerformance[i] = rand.Next(1, 5);
+            }
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname1 { get => surname1; set => surname1 = value; }
+        public string Surname2 { get => surname2; set => surname2 = value; }
+        public string Group { get => group; set => group = value; }
+        public int[] AcademicPerformance { get => academicPerformance; }
+
+        public 
+    }
+}
