344de4d6f7122a87b7cd96908e4ebeca80c4ccb3
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2024-04-09 20:32:31 +0300

    asd

---
+++ Project2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s;
+            try
+            {
+                s = textBox1.Text;
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            label1.Text = "Заглавных: " + s.Count(m => char.IsUpper(m)).ToString() + "\nСтрочных: " + s.Count(m => char.IsLower(m)).ToString();
+        }
+    }
+}
